a7ebd66dfe05dd675cb31202c439c54b55ab8747
Ann <shokh.anna@mail.ru>
2022-11-20 14:52:48 +0300

    Fill comments template with data

--- src/view/popup/film-popup-comments-template.js
+++ src/view/popup/film-popup-comments-template.js
@@ -1,60 +1,30 @@
+
+const createFilmPopupCommentTemplate = (popupComment) => {
+  const {author, comment, date, emotion} = popupComment;
+
+  return (
+    `<li class="film-details__comment">
+         <span class="film-details__comment-emoji">
+           <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-smile">
+         </span>
+         <div>
+           <p class="film-details__comment-text">${comment}</p>
+           <p class="film-details__comment-info">
+             <span class="film-details__comment-author">${author}</span>
+             <span class="film-details__comment-day">${date}</span>
+             <button class="film-details__comment-delete">Delete</button>
+           </p>
+         </div>
+      </li>`
+  );
+};
+
+
+
 export const createFilmPopupCommentsTemplate = (popupComments) => {
-
-  const {author, comment, date, emotion} = popupComments;
-
-  return (`<ul class="film-details__comments-list">
-  <li class="film-details__comment">
-    <span class="film-details__comment-emoji">
-      <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
-    </span>
-    <div>
-      <p class="film-details__comment-text">Interesting setting and a good cast</p>
-      <p class="film-details__comment-info">
-        <span class="film-details__comment-author">Tim Macoveev</span>
-        <span class="film-details__comment-day">2019/12/31 23:59</span>
-        <button class="film-details__comment-delete">Delete</button>
-      </p>
-    </div>
-  </li>
-  <li class="film-details__comment">
-    <span class="film-details__comment-emoji">
-      <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji-sleeping">
-    </span>
-    <div>
-      <p class="film-details__comment-text">Booooooooooring</p>
-      <p class="film-details__comment-info">
-        <span class="film-details__comment-author">John Doe</span>
-        <span class="film-details__comment-day">2 days ago</span>
-        <button class="film-details__comment-delete">Delete</button>
-      </p>
-    </div>
-  </li>
-  <li class="film-details__comment">
-    <span class="film-details__comment-emoji">
-      <img src="./images/emoji/puke.png" width="55" height="55" alt="emoji-puke">
-    </span>
-    <div>
-      <p class="film-details__comment-text">Very very old. Meh</p>
-      <p class="film-details__comment-info">
-        <span class="film-details__comment-author">John Doe</span>
-        <span class="film-details__comment-day">2 days ago</span>
-        <button class="film-details__comment-delete">Delete</button>
-      </p>
-    </div>
-  </li>
-  <li class="film-details__comment">
-    <span class="film-details__comment-emoji">
-      <img src="./images/emoji/angry.png" width="55" height="55" alt="emoji-angry">
-    </span>
-    <div>
-      <p class="film-details__comment-text">Almost two hours? Seriously?</p>
-      <p class="film-details__comment-info">
-        <span class="film-details__comment-author">John Doe</span>
-        <span class="film-details__comment-day">Today</span>
-        <button class="film-details__comment-delete">Delete</button>
-      </p>
-    </div>
-  </li>
-</ul>
-  `);
-}
+  return (
+    `<ul class="film-details__comments-list">
+      ${popupComments.map(createFilmPopupCommentTemplate).join('')}
+    </ul>`
+  );
+};
