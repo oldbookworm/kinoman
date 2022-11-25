4039b571a9d615d0d84ea131331036b2170782a5
Ann <shokh.anna@mail.ru>
2022-11-25 22:11:59 +0300

    Add no-film-view

---
+++ src/view/no-films-view.js
@@ -0,0 +1,25 @@
+import {createElement} from '../render.js';
+
+const noFilmsTemplate = () => (
+  `<h2 class="films-list__title">There are no movies in our database</h2>`
+  );
+
+  export default class NoFilmsView {
+    #element = null;
+
+    get template() {
+      return noFilmsTemplate();
+    }
+
+    get element() {
+      if (!this.#element) {
+        this.#element = createElement(this.template);
+      }
+
+      return this.#element;
+    }
+
+    removeElement() {
+      this.#element = null;
+    }
+  }
